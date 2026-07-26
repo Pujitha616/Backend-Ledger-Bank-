88900794e890f92e3a4b22275e8c8287cc5f9d2e
Pujitha616 <205028.main@sasi.edu.in>
2026-07-26 15:25:49 +0530

    Initial commit for backend-ledger

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+const userModel=require('../models/user.model');
+const jwt=require('jsonwebtoken');
+
+
+/** 
+* - user register controller
+* - POST /api/auth/register
+*/
+
+async function userRegisterController(req,res){
+  const {email,name,password}=req.body;
+  const isExists=await userModel.findOne({
+    email:email
+})
+
+  if(isExists){
+    return res.status(422).json({
+        success:false,
+        message:"Email already exists",
+        status:"failed"
+    })
+  }
+  const user=await userModel.create({
+    email,
+    name,
+    password
+  })
+    const token=jwt.sign({userid:user._id},process.env.JWT_SECRET,{expiresIn:"3d"})
+    res.cookies("token",token)
+    res.status(201).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token
+    })  
+}
+
+module.exports = {
+  userRegisterController,
+};
